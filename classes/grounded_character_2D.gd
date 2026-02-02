d3ecdd55b76a0e1ba7aef52313aad4a9bfe59db2
itsMewJAM <itsmewjam@gmail.com>
2026-02-01 17:32:31 -0800

    Fall down off ledges

--- classes/grounded_character_2D.gd
+++ classes/grounded_character_2D.gd
@@ -9,7 +9,8 @@ extends CharacterBody2D
 @export var gravity := 400.0
 
 var direction := 0
-var trying_jump
+var trying_jump := false
+var jumping := false
 
 func _physics_process(delta: float) -> void:
 	#Falling
@@ -17,13 +18,20 @@ func _physics_process(delta: float) -> void:
 	
 	#Try Grounded Movement
 	if is_on_floor():
-		#Update horizontal movement direction based on inputted direction
+		# Set jumping to false for returning to the ground
+		jumping = false
+		
+		# Update horizontal movement direction based on inputted direction
 		velocity.x = direction * speed_grounded
 		
 		# Try Jump from ground
 		if trying_jump:
 			velocity.y = jump_force
 			velocity.x = direction * speed_airborne
+			jumping = true # Will remain false if fallen from a ledge
+	else:
+		if not jumping:
+			velocity.x = 0
 	
 	#Do Movement using velocity
 	move_and_slide()

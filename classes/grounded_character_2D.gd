fa9a6cf859c0c7181e5d9bd5e4e18344a4ef8964
itsMewJAM <itsmewjam@gmail.com>
2026-02-01 17:23:18 -0800

    Air movement locked

--- classes/grounded_character_2D.gd
+++ classes/grounded_character_2D.gd
@@ -3,25 +3,27 @@ extends CharacterBody2D
 
 # Should be usable for both player and enemy.
 
-@export var speed := 100.0
-@export var jump_force := -180.0
+@export var speed_grounded := 100.0
+@export var speed_airborne := 75.0
+@export var jump_force := -200.0
 @export var gravity := 400.0
 
 var direction := 0
 var trying_jump
 
 func _physics_process(delta: float) -> void:
-	#Horizontal Movement
-	velocity.x = direction * speed
-	
 	#Falling
 	velocity.y += gravity * delta
 	
-	#Attempt Jump
-	if trying_jump and is_on_floor():
-		#print("JUMPING")
-		#position.y -= 11
-		velocity.y = jump_force
+	#Try Grounded Movement
+	if is_on_floor():
+		#Update horizontal movement direction based on inputted direction
+		velocity.x = direction * speed_grounded
+		
+		# Try Jump from ground
+		if trying_jump:
+			velocity.y = jump_force
+			velocity.x = direction * speed_airborne
 	
 	#Do Movement using velocity
 	move_and_slide()

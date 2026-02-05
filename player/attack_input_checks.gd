7523f3085e94fcc31db8e90c273ef19ef7e29d88
Joshua Kim <joshua.hyunki.kim@gmail.com>
2026-02-04 21:08:38 -0800

    smoothening the input system. added correct 22x logic

--- player/attack_input_checks.gd
+++ player/attack_input_checks.gd
@@ -1,45 +1,56 @@
 extends Node
 
+# Helper function to support the input checking logic
+# 
+static func _last_n_input(
+	input_registry,
+	find_input: int,
+	excluded_inputs,
+	start: int,
+	between: int
+) -> int:
+	var index = -1
+	for i in range(start, start+between):
+		# Avoid out-of-range errors
+		if i >= 30: return -1
+		if input_registry[i] == find_input:
+			index = i 
+		if int(input_registry[i]) in excluded_inputs: return -1
+	return index
+
 # Check for 623X input. Conditions: 
-# (3) must have been first input in the past 6 frames, and none of the inputs can be more than 6 frames apart.
-# (2) must be input before (3) with no other inputs in between
+# (3) must have been first input in the past 6 frames
+# (2) must be input before (3) with no other inputs in between, within 6 frames
 # There cannot be a backwards input (1/4/7) between (6) and (2) 
 static func check_623X(input_registry):
-	var hit6 = false
-	var hit2 = false
-	var between = 0
-	var idx = 0
-	print(input_registry)
-	for input in input_registry:
-		if !hit6:
-			if input == 6:
-				hit6 = true
-				print('hit6')
-				between = -1
-		elif !hit2:
-			if input == 6: between = -1
-			if input == 1 or input == 4 or input == 7: return false
-			if input == 2:
-				print('hit2')
-				hit2 = true
-				between = -1
-		else:
-			if input == 3: 
-				print('done')
-				return true if input_registry.size() - idx <= 6 else false
-			elif input != 2: return false
-		between += 1
-		idx += 1
-		if hit6 and between > 10: return false
-	
-	return false
+	var hit3 = _last_n_input(input_registry, 3, [], 0, 6)
+	if hit3 == -1: return false
+	var hit2 = _last_n_input(input_registry, 2, [1,4,5,6,7,8,9], hit3, 6)
+	if hit2 == -1: return false
+	var hit6 = _last_n_input(input_registry, 6, [1,4,7], hit2, 10)
+	if hit6 == -1: return false
+	return true
 
 # Check for 236X input. Conditions: 
-# (6) must have been input in the past 6 frames, and none of the inputs can be more than 6 frames apart.
-# (2) must be input before (3) with no other inputs in between
-# There cannot be a backwards input (1/4/7) between (6) and (2) 
+# (6) must have been first input in the past 6 frames, and none of the inputs can be more than 6 frames apart.
+# (There cannot be any other inputs 
 static func check_236X(input_registry):
-	pass	
+	var hit6 = _last_n_input(input_registry, 6, [], 0, 6)
+	if hit6 == -1: return false
+	var hit3 = _last_n_input(input_registry, 3, [1,2,4,5,7,8,9], hit6, 6)
+	if hit3 == -1: return false
+	var hit2 = _last_n_input(input_registry, 2, [1,4,5,6,7,8,9], hit3, 6)
+	if hit2 == -1: return false
+	return true
 
 static func check_22X(input_registry):
-	pass
+	var hit2 = _last_n_input(input_registry, 2, [], 0, 2)
+	if hit2 == -1: return false
+	var hit_neutral = max(
+		_last_n_input(input_registry, 5, [], hit2, 6),
+		_last_n_input(input_registry, 8, [], hit2, 6)
+	)
+	if hit_neutral == -1: return false
+	var hit2_2 = _last_n_input(input_registry, 2, [], hit_neutral, 2)
+	if hit2_2 == -1: return false
+	return true
